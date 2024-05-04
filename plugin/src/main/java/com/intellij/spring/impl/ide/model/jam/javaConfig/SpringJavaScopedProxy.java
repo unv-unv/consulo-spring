c221731835198300828974a4f742ce4cccfe83e8
Valery Semenchuk <vistall.valeriy@gmail.com>
2024-05-04 21:48:13 +0400

    drop dead code

--- plugin/src/main/java/com/intellij/spring/impl/ide/model/jam/javaConfig/SpringJavaScopedProxy.java
+++
@@ -1,6 +0,0 @@
-package com.intellij.spring.impl.ide.model.jam.javaConfig;
-
-import com.intellij.jam.model.common.CommonModelElement;
-
-public interface SpringJavaScopedProxy extends CommonModelElement {
-}
