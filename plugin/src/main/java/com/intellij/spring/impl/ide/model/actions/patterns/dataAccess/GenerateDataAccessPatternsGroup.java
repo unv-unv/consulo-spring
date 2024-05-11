ac72e7ca43938e6d2751c6357289dda80c55c0bd
Valery Semenchuk <vistall.valeriy@gmail.com>
2024-05-11 18:58:47 +0400

    drop usages dead icons

--- plugin/src/main/java/com/intellij/spring/impl/ide/model/actions/patterns/dataAccess/GenerateDataAccessPatternsGroup.java
+++ plugin/src/main/java/com/intellij/spring/impl/ide/model/actions/patterns/dataAccess/GenerateDataAccessPatternsGroup.java
@@ -1,18 +1,18 @@
 package com.intellij.spring.impl.ide.model.actions.patterns.dataAccess;
 
-import consulo.application.AllIcons;
-import consulo.ui.ex.action.AnActionEvent;
-import consulo.ui.ex.action.DefaultActionGroup;
 import com.intellij.spring.impl.ide.SpringBundle;
 import com.intellij.spring.impl.ide.model.actions.GenerateSpringDomElementAction;
 import com.intellij.spring.impl.ide.model.actions.generate.SpringBeanGenerateProvider;
 import com.intellij.spring.impl.ide.model.actions.patterns.PatternIcons;
+import consulo.ui.ex.action.AnActionEvent;
+import consulo.ui.ex.action.DefaultActionGroup;
+import consulo.ui.image.Image;
 
 public class GenerateDataAccessPatternsGroup extends DefaultActionGroup {
 
   public GenerateDataAccessPatternsGroup() {
-    add(new GenerateSpringDomElementAction(new SpringBeanGenerateProvider(SpringBundle.message("spring.patterns.data.access.data.source"), "datasource"), AllIcons.Nodes.DataSource)) ;
-    add(new GenerateSpringDomElementAction(new SpringBeanGenerateProvider(SpringBundle.message("spring.patterns.data.access.jndi.data.source"), "jndi-datasource"), AllIcons.Nodes.DataSource)) ;
+    add(new GenerateSpringDomElementAction(new SpringBeanGenerateProvider(SpringBundle.message("spring.patterns.data.access.data.source"), "datasource"), Image.empty())) ;
+    add(new GenerateSpringDomElementAction(new SpringBeanGenerateProvider(SpringBundle.message("spring.patterns.data.access.jndi.data.source"), "jndi-datasource"), Image.empty())) ;
     add(new GenerateSpringDomElementAction(new SpringBeanGenerateProvider(SpringBundle.message("spring.patterns.data.access.transaction.manager"), "transaction-manager"), PatternIcons.TRANSACTION_MANAGER_ICON)) ;
     addSeparator();
     add(new GenerateSpringDomElementAction(new SpringBeanGenerateProvider(SpringBundle.message("spring.patterns.data.access.hibernate.session.factory"), "hibernatefactory"), PatternIcons.HIBERNATE_ICON)) ;
