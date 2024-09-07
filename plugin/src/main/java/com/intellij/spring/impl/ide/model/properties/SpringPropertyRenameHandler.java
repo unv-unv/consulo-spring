4be670bd1d851a234420a073ae2330e4605bdd3e
Valery Semenchuk <vistall.valeriy@gmail.com>
2024-09-07 18:23:50 +0400

    add #getActionTitleValue()

--- plugin/src/main/java/com/intellij/spring/impl/ide/model/properties/SpringPropertyRenameHandler.java
+++ plugin/src/main/java/com/intellij/spring/impl/ide/model/properties/SpringPropertyRenameHandler.java
@@ -8,6 +8,8 @@ import com.intellij.java.impl.psi.impl.beanProperties.BeanProperty;
 import com.intellij.java.impl.refactoring.rename.BeanPropertyRenameHandler;
 import consulo.annotation.component.ExtensionImpl;
 import consulo.dataContext.DataContext;
+import consulo.localize.LocalizeValue;
+import jakarta.annotation.Nonnull;
 
 import javax.annotation.Nullable;
 
@@ -16,8 +18,15 @@ import javax.annotation.Nullable;
  */
 @ExtensionImpl
 public class SpringPropertyRenameHandler extends BeanPropertyRenameHandler {
-  @Nullable
-  protected BeanProperty getProperty(DataContext context) {
-    return SpringPropertiesUtil.getBeanProperty(context);
-  }
+    @Override
+    @Nullable
+    protected BeanProperty getProperty(DataContext context) {
+        return SpringPropertiesUtil.getBeanProperty(context);
+    }
+
+    @Nonnull
+    @Override
+    public LocalizeValue getActionTitleValue() {
+        return LocalizeValue.localizeTODO("Spring Property Rename...");
+    }
 }
--- plugin/src/main/java/com/intellij/spring/impl/ide/model/xml/custom/SpringCustomBeanRenameHandler.java
+++ plugin/src/main/java/com/intellij/spring/impl/ide/model/xml/custom/SpringCustomBeanRenameHandler.java
@@ -9,7 +9,9 @@ import consulo.language.editor.refactoring.rename.PsiElementRenameHandler;
 import consulo.language.editor.refactoring.rename.RenameHandler;
 import consulo.language.psi.PsiElement;
 import consulo.language.psi.PsiFile;
+import consulo.localize.LocalizeValue;
 import consulo.project.Project;
+import consulo.ui.annotation.RequiredUIAccess;
 import consulo.ui.ex.awt.Messages;
 import consulo.xml.psi.xml.XmlAttribute;
 
@@ -17,36 +19,49 @@ import javax.annotation.Nonnull;
 
 @ExtensionImpl
 public class SpringCustomBeanRenameHandler implements RenameHandler {
-
-  public boolean isAvailableOnDataContext(DataContext dataContext) {
-    return false;
-  }
-
-  public boolean isRenaming(DataContext dataContext) {
-    PsiElement element = dataContext.getData(LangDataKeys.PSI_ELEMENT);
-    return element instanceof CustomBeanFakePsiElement;
-  }
-
-  public void invoke(@Nonnull Project project, Editor editor, PsiFile file, DataContext dataContext) {
-    final PsiElement element = dataContext.getData(LangDataKeys.PSI_ELEMENT);
-    doInvoke(project, editor, element);
-  }
-
-  private static void doInvoke(final Project project, final Editor editor, final PsiElement element) {
-    final XmlAttribute idAttribute = ((CustomBeanFakePsiElement)element).getBean().getIdAttribute();
-    if (idAttribute == null) {
-      final int i = Messages
-        .showOkCancelDialog(project,
-                            SpringBundle.message("custom.bean.no.id"),
-                            SpringBundle.message("custom.bean.no.id.title"),
-                            Messages.getWarningIcon());
-      if (i != 0) return;
+    @Nonnull
+    @Override
+    public LocalizeValue getActionTitleValue() {
+        return LocalizeValue.localizeTODO("Spring Bean Rename...");
     }
 
-    PsiElementRenameHandler.invoke(element, project, element, editor);
-  }
+    @Override
+    public boolean isAvailableOnDataContext(DataContext dataContext) {
+        return false;
+    }
 
-  public void invoke(@Nonnull Project project, @Nonnull PsiElement[] elements, DataContext dataContext) {
-  }
+    @Override
+    public boolean isRenaming(DataContext dataContext) {
+        PsiElement element = dataContext.getData(LangDataKeys.PSI_ELEMENT);
+        return element instanceof CustomBeanFakePsiElement;
+    }
+
+    @RequiredUIAccess
+    @Override
+    public void invoke(@Nonnull Project project, Editor editor, PsiFile file, DataContext dataContext) {
+        final PsiElement element = dataContext.getData(LangDataKeys.PSI_ELEMENT);
+        doInvoke(project, editor, element);
+    }
+
+    private static void doInvoke(final Project project, final Editor editor, final PsiElement element) {
+        final XmlAttribute idAttribute = ((CustomBeanFakePsiElement) element).getBean().getIdAttribute();
+        if (idAttribute == null) {
+            final int i = Messages
+                .showOkCancelDialog(project,
+                    SpringBundle.message("custom.bean.no.id"),
+                    SpringBundle.message("custom.bean.no.id.title"),
+                    Messages.getWarningIcon());
+            if (i != 0) {
+                return;
+            }
+        }
+
+        PsiElementRenameHandler.invoke(element, project, element, editor);
+    }
+
+    @RequiredUIAccess
+    @Override
+    public void invoke(@Nonnull Project project, @Nonnull PsiElement[] elements, DataContext dataContext) {
+    }
 
 }
